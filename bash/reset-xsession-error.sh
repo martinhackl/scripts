2c8873244456464e20cea64f039ac7d6b7c920fd
Martin Hackl <git@martinhackl.com>
2020-09-10 18:48:38 +0200

    Disable xsession-error script

---
+++ bash/reset-xsession-error.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#===============================================================================
+#
+#         NAME: reset-xsession-error.sh
+#
+#        USAGE: reset-xsession-error.sh [domain name]
+#
+#  DESCRIPTION: Disable logging to .xsession-error. This error log can grow very
+#               large and in extreme cases will use any space left on the drive.
+#
+# REQUIREMENTS: /
+#      VERSION: 1.0.0
+#    REFERENCE: https://askubuntu.com/a/272495
+#
+#===============================================================================
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+sed -i '/^ERRFILE=\$HOME\/\.xsession-errors$/ s/^/#/' /etc/X11/Xsession
