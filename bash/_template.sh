26b1082195438f0f897b011a31ce3a6a5d40aae7
Martin Hackl <git@martinhackl.com>
2020-09-10 18:50:48 +0200

    Init base template

---
+++ bash/_template.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#===============================================================================
+#
+#         NAME: TODO.sh
+#
+#        USAGE: TODO.sh
+#
+#  DESCRIPTION: TODO
+#
+# REQUIREMENTS: TODO || /
+#      VERSION: 1.0.0
+#    REFERENCE: TODO || remove line
+#
+#===============================================================================
+
+set -o errexit
+set -o pipefail
+set -o nounset
